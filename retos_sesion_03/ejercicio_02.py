9dff3231ea16c4484ade564edc01e366fcab5e53
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-10 15:23:16 -0400

    Arreglé mi ejercicio_02.py

--- retos_sesion_03/ejercicio_02.py
+++ retos_sesion_03/ejercicio_02.py
@@ -1,19 +1,11 @@
-print(" CONVERSACIÓN ENTRE LUCAS Y MAGDALENA")
-
-print("Lucas: Buenos días Magda")
-print("Magdalena: Hola Lucas, ¿como te encuentras?")
+print("Persona: ¡Hola, michi!")
+print("Gato: Miau")
 print("")
-print("Lucas: Me encuentro bien, con un poco de sueño ¿como se encuentra el clima por allá?")
-print("Magdalena: Terrible, hace mucho frio y no para de llover T-T ")
+print("Persona: ¿Tienes hambre?")
+print("Gato: Miau miau (¡Sí, mucho!)")
 print("")
-
-print("Lucas: ¡Que terrible! espero todo mejore pronto por allá")
-print("Magdalena: Espero que si, con este clima no tengo ganas de ir al colegio")
+print("Persona: Está bien, vamos a comer.")
+print("Gato: Miau~ (Gracias, humano)")
 print("")
-
-print("Lucas: No no no, el clima no puede ser excusa para no ir a clases :0")
-print("Magdalena: Cierto cierto, es más, pronto ya debería alistarme para ir")
-print("")
-
-print("Lucas: Entonces no te interrumpo más, charlamos más tarde, espero tengas un bonito día")
-print("Magdalena: Igualmente a ti, que te vaya bien en colegio")
+print("Persona: Hasta luego, michi.")
+print("Gato: Miau.")
