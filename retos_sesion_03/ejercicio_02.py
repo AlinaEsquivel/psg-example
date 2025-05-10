65a552dbd1f3ae229a96528b92214b871e854249
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-10 14:33:30 -0400

    Estoy agregando el ejercicio 2

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,19 @@
+print(" CONVERSACIÓN ENTRE LUCAS Y MAGDALENA")
+
+print("Lucas: Buenos días Magda")
+print("Magdalena: Hola Lucas, ¿como te encuentras?")
+print("")
+print("Lucas: Me encuentro bien, con un poco de sueño ¿como se encuentra el clima por allá?")
+print("Magdalena: Terrible, hace mucho frio y no para de llover T-T ")
+print("")
+
+print("Lucas: ¡Que terrible! espero todo mejore pronto por allá")
+print("Magdalena: Espero que si, con este clima no tengo ganas de ir al colegio")
+print("")
+
+print("Lucas: No no no, el clima no puede ser excusa para no ir a clases :0")
+print("Magdalena: Cierto cierto, es más, pronto ya debería alistarme para ir")
+print("")
+
+print("Lucas: Entonces no te interrumpo más, charlamos más tarde, espero tengas un bonito día")
+print("Magdalena: Igualmente a ti, que te vaya bien en colegio")
