1112aa86aa6e622c25c2cb2bf3757a941ab7c8b3
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-11 20:57:39 -0400

    Sesion 04

---
+++ sesion04.py
@@ -0,0 +1,12 @@
+# Example of execution flow
+# Instruction 1
+print ("Start of the program")
+
+# Instruction 2
+print ("Print secont instruction")
+
+# Instruction 3
+print ("Hello, world!")
+
+# instruction 4
+print ("End of the program")
