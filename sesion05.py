653ef0d4c62c0683b8465d8e9d80fc3a95ba6dcb
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-19 16:09:29 -0400

    clase 5

---
+++ sesion05.py
@@ -0,0 +1,65 @@
+# valor 10 entero
+print (10)
+print ( type (10))
+
+print ("")
+print ("otra variable")
+variable = 100
+print (variable)
+print ( type (variable))
+
+print ("")
+print ("variable 2")
+variable_2 = int (20)
+print (variable_2)
+print ( type (variable_2))
+
+print("")
+# Vador 10 en base decimal
+print ("Base decimal")
+print (10)
+# Vador 10 en base binaria
+print ("Base binaria")
+print (0b1010)
+# Vador 10 en base octal
+print ("Base octal")
+print (0o12)
+# Vador 10 en base hexadecimal
+print ("Base hexadecimal")
+print (0xa)
+
+# procesar numeros grandes con python es mas facil
+print("")
+variable_3 = 1234567890123456789012345678901234567890
+print (variable_3)
+print (type (variable_3))
+
+# variable 0.100546
+print ("")
+variable_4 = 0.100546
+print (variable_4)
+print (type (variable_4))
+
+#variable 1 float
+print ("")
+variable_5 = float (1)
+print (variable_5)
+print (type (variable_5))
+
+# precision max de 17 decimales
+print ("")
+variable_7 = 0.9999999999999999
+print (variable_7)
+print (type (variable_7))
+
+# errores a partir del 18
+print ("")
+variable_8 = 0.99999999999999999
+print (variable_8)
+print (type (variable_8))
+
+# variable 2-03-3 float
+print ("")
+variable_6 = 2.0e-3
+print (variable_6)
+print (type (variable_6))
---
+++ sesion05_ejercicio1.py
@@ -0,0 +1,25 @@
+contador_Min = 300
+segundos_Mas = 3600
+
+minutos = segundos_Mas / 60
+horas_casi = contador_Min + minutos
+
+horas = horas_casi / 60 
+ 
+print (horas)
+
+print("")
+print ("Operadores de comparación")
+comparar = 10
+
+print (comparar < 10)
+print (comparar > 10)
+print (comparar == 10)
+print (comparar <= 10)
+print (comparar >= 10)
+print (comparar != 10)
+print("")
+# comparar
+print ("2" == 2)
+print (2.0 is 2)
+print (2 == 2.0)
