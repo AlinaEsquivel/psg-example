4845c326d0c190564387b7b07ba2f6cc8a0a60e1
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-19 20:39:25 -0400

    Conversion de °F a °C

---
+++ retos_sesion_05/ejercicio_02.py
@@ -0,0 +1,25 @@
+# Escribe un programa en Python que convierta las
+# siguientes temperaturas de grados Fahrenheit a 
+# grados Celsius: 25 ºF, 300 ºF, 76 ºF
+
+grado_F1 = 25
+grado_F2 = 300
+grado_F3 = 76
+
+# 1er caso: de 25 ºF a °C
+print ("")
+print ("Los 25° F en °C es:")
+Celsius_1 = (25 -32)*(5/9)
+print (Celsius_1)
+
+# 2do caso: de 300 ºF a °C
+print ("")
+print ("Los 300° F en °C es:")
+Celsius_2 = (300 -32)*(5/9)
+print (Celsius_2)
+
+# 3ro caso: de 76 ºF a °C
+print ("")
+print ("Los 76° F en °C es:")
+Celsius_3 = (76 -32)*(5/9)
+print (Celsius_3)
