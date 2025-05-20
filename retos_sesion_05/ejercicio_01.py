1ee39d9027253a2369efb077644214259bd1cf4e
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-19 20:19:27 -0400

    costo de 12 productos

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,9 @@
+# Escribe un programa en Python que calcule el 
+# total a pagar si se compran 12 productos 
+# iguales donde cada uno cuesta 3.75 USD.
+
+prod_1 = 3.75
+total = prod_1 * 12
+print ("")
+print ("El total de los 12 productos es:")
+print (prod_1)
