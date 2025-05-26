cf625978e940c5fc4025a46b11f44bd0cc22b260
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-26 00:30:09 -0400

    tarea 5

---
+++ retos_sesion_06/ejercicio_05.py
@@ -0,0 +1,9 @@
+num1 = 44
+num2 = 98
+num3 = 111
+num4 = 333
+print ("Primeros pares: Pares")
+print (bool (num1%2==0 and num2%2==0))
+print("")
+print ("Primeros pares: Impares")
+print (bool (num3%2==1 and num4%2==1))
