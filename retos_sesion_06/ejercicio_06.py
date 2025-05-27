f2423f4ad283716d230c611c213d24201d078f75
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-27 17:04:07 -0400

    ejercicio 6 :D

---
+++ retos_sesion_06/ejercicio_06.py
@@ -0,0 +1,7 @@
+# ¿El cubo de 300 se encuentra en el siguiente rango? 
+# [N > 0 & N < 27_000_000]
+
+cubo = 300
+en_el_rango = 300>0 and 300<27000000
+print ("¿el cubo esta en el rango?")
+print (bool (en_el_rango))
