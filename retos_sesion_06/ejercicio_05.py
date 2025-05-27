ad0ebe3bf738966595ad49dc628064cbd931c3fa
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-27 16:51:20 -0400

    Ejercicio 5

--- retos_sesion_06/ejercicio_05.py
+++ retos_sesion_06/ejercicio_05.py
@@ -2,8 +2,19 @@ num1 = 44
 num2 = 98
 num3 = 111
 num4 = 333
-print ("Primeros pares: Pares")
+print ("Pares")
 print (bool (num1%2==0 and num2%2==0))
+print (bool (num1%2==0 and num3%2==0))
+print (bool (num1%2==0 and num4%2==0))
+print (bool (num2%2==0 and num3%2==0))
+print (bool (num2%2==0 and num4%2==0))
+print (bool (num3%2==0 and num4%2==0))
+
 print("")
-print ("Primeros pares: Impares")
+print ("Impares")
+print (bool (num1%2==1 and num2%2==1))
+print (bool (num1%2==1 and num3%2==1))
+print (bool (num1%2==1 and num4%2==1))
+print (bool (num2%2==1 and num3%2==1))
+print (bool (num2%2==1 and num4%2==1))
 print (bool (num3%2==1 and num4%2==1))
