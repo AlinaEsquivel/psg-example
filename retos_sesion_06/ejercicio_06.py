f79be0e928d54c3f82d008a11d8bf110088f84b0
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-05-27 20:10:36 -0400

    Ejercicio 6

--- retos_sesion_06/ejercicio_06.py
+++ retos_sesion_06/ejercicio_06.py
@@ -1,7 +1,7 @@
 # ¿El cubo de 300 se encuentra en el siguiente rango? 
 # [N > 0 & N < 27_000_000]
 
-cubo = 300
-en_el_rango = 300>0 and 300<27000000
-print ("¿el cubo esta en el rango?")
+num = 300**3
+en_el_rango = num>0 and num<27_000_000
+print (num, "= ¿el numero esta en el rango?")
 print (bool (en_el_rango))
