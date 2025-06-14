cfdcd36617631a109ffb5400c8efe97b5fe7aed2
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-06-14 14:32:17 -0400

    ejercicio  :D

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,19 @@
+print ("")
+
+#Coordenadas
+coordenadas1 = (-20, -20)
+x1, y1 = coordenadas1
+print ("coordenadas 1:", coordenadas1)
+
+coordenadas2 = (40, 20)
+x2, y2 = coordenadas2
+print ("coordenadas 2:", coordenadas2)
+
+#Encontrar punto medio de x - y
+punto_medio_x = (x1 + x2) / 2
+punto_medio_y = (y1 + y2) / 2
+
+#almacenar en tupla
+tupla = (punto_medio_x, punto_medio_y)
+print ("punto medio", (tupla))
+
