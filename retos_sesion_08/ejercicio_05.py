8bdea6d39b8308bbedf7061dc51a97b73a97fa6c
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-06-14 14:18:37 -0400

    ejercicio5

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,8 @@
+#Generar una tupla anidada e imprimri el resultado
+tupla_anidada = (
+    ("Jhon", 591123456, "Villa fátima"),
+    ("Jane", 591546324, "Obrajes"),
+    ("Dogg", 591222421, "Miraflores"),
+    ("Kity", 591753159, "Achumani")
+)
+print (tupla_anidada)
