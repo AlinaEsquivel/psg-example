83bba010cfe1495d1b098367d71b185257b4727e
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-06-14 13:58:15 -0400

    ejercicio 3 :'3

---
+++ retos_sesion_08/ejercicio_03.py
@@ -0,0 +1,15 @@
+#Ingresa una pregunta por teclado y almacena el valor en una tupla
+#Concatena las tupla
+#('¿', ) + pregunta + ('?', )
+#Imprime el resultado concatenado
+#Repite la tupla concatenada 2 veces e imprime el nuevo resultad#
+print ("")
+print ("Pregunta:")
+pregunta = ("Tienes gatitos", )
+print (pregunta)
+concatenar = ("¿", ) + pregunta + ("?", )
+print ("pregunta con signos ¿?")
+print (concatenar)
+print ("pregunta repetida")
+print (concatenar*2)
+
