e148fd76ffa6559ca06bcd637dd99c9d3fb2bdec
Alina Esquivel <alina.esquivel.68104365@gmail.com>
2025-06-13 21:23:53 -0400

    sesion 7

---
+++ sesion07.py
@@ -0,0 +1,25 @@
+simple = 'Mi cadena permite comillas "dobles" en una sola línea'
+doble  = "Mi cadena permite comillas 'simples' en una sola línea"
+triple_simple = '''Mi cadena
+permite contenido 
+en varias líneas y comillas "dobles" '''
+triple_doble = """Mi cadena
+permite contenido 
+en varias líneas y comillas 'simples' """
+print (simple)
+print (doble)
+print (triple_simple)
+print (triple_doble)
+print ("")
+entero = str(1)
+flotante = str(1E-3)
+hexadecimal = str(0xA)
+booleano = str (True)
+print (entero)
+print (flotante)
+print (hexadecimal)
+print (booleano)
+print (type(entero))
+print (type(flotante))
+print (type(hexadecimal))
+print (type(booleano))
